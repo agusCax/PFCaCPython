5f42296c587b76c21487f1781dab81564b83a6da
agustina <agus.es.17@gmail.com>
2024-05-18 15:53:52 -0300

    primera actualizacion de seccion artesanos

---
+++ script.js
@@ -0,0 +1,23 @@
+const openEls = document.querySelectorAll("[data-open]");
+const isVisible = "is-visible";
+for(const el of openEls) {
+    el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+    });
+}
+
+const closeEls = document.querySelectorAll("[data-close]");
+for (const el of closeEls) {
+    el.addEventListener("click", function() {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    });
+}
+
+
+document.addEventListener("keyup", e => {
+    if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+});
+
